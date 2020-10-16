e08b5307b3c1141aa4a9be579a47906522b33ea9
Aika Takada <aktsm0syn0pgmail.com>
2020-10-16 20:25:48 +0900

    Updates

---
+++ precache-manifest.f01b15ee58d205e1d26ff73769e6f8af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae622be1e9210f2e9d0c0ae5a05cd778",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "52ac4d15ab8ba4021ee2",
+    "url": "/my-portfolio/static/css/main.e86783ac.chunk.css"
+  },
+  {
+    "revision": "ab6b1c4afad74a9b9671",
+    "url": "/my-portfolio/static/js/2.414eaded.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.414eaded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52ac4d15ab8ba4021ee2",
+    "url": "/my-portfolio/static/js/main.8531e7cb.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "293b27893b426067e4f129e6f638b24b",
+    "url": "/my-portfolio/static/media/my-portfolio.293b2789.png"
+  },
+  {
+    "revision": "4718921312e9f76059039f8b7ce37c6a",
+    "url": "/my-portfolio/static/media/todolist.47189213.png"
+  }
+]);
